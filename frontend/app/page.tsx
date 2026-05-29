2418a6ca43e75f47852709fdab0496d698e8a337
Devansh Shukla <devanshc.shukla@gmail.com>
2026-05-30 02:00:54 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
     setStatus("Processing lead...");
     
     try {
-        const res = await fetch('http://localhost:8000/process', {
+        const res = await fetch('https://email-automation-b5ey.onrender.com/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 
